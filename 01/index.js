cf815ea59260372cf6785bc3ec958c6c9f36b001
Moisa0 <95927203+Moisa0@users.noreply.github.com>
2023-08-30 23:45:08 -0300

    exercicios resolvidos com muito suor

--- 01/index.js
+++ 01/index.js
@@ -8,7 +8,7 @@ const prova = {
             correta: "b"
         },
         {
-            resposta: "c",
+            resposta: "b",
             correta: "c"
         },
         {
@@ -16,13 +16,32 @@ const prova = {
             correta: "b"
         },
         {
-            resposta: "b",
+            resposta: "a",
             correta: "b"
         },
         {
             resposta: "c",
-            correta: "c"
+            correta: "b"
         }
     ]
 };
 
+function corrigirProva(simulado){
+    let nota = 0
+    for(alternativas of simulado.questoes){
+        if(alternativas.resposta === alternativas.correta){
+            nota += 2
+        }else{
+            nota += 0
+        }
+    }if(nota === 0){
+        console.log(`O aluno(a) ${simulado.aluno} acertou nenhuma questão e obteve nota 0`)
+    }else if(nota === 2){
+        console.log(`O aluno(a) ${simulado.aluno} acertou 1 questão e obteve nota ${nota}`)
+    }else{
+        console.log(`O aluno(a) ${simulado.aluno} acertou ${nota/2} questões e obteve nota ${nota}`)
+    }
+
+}
+
+corrigirProva(prova)
